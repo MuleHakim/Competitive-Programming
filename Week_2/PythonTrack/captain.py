3bca48748fce94ce200a8f31823fc8d9a3a27705
MuleHakim <mulukenhakim97@gmail.com>
2022-12-27 00:42:42 +0300

    update

--- Week_2/PythonTrack/captain.py
+++
@@ -1,5 +0,0 @@
-from collections import Counter
-k = int(input())
-room = list(map(int, input().split()))
-counter = Counter(room)
-print(list(counter.keys())[-1])
--- Week_2/PythonTrack/hashtuple.py
+++
@@ -1,5 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-n = int(input())
-t = tuple(map(int,input().split(None,n)[:n]))
-print(t)
-print(hash(t))
