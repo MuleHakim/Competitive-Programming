85ca96d7988d56654d74b02c719b19c9a0b076ca
MuleHakim <mulukenhakim97@gmail.com>
2023-01-01 17:24:59 +0300

    design bank system

---
+++ Week_3/design.py
@@ -0,0 +1,28 @@
+class Bank:
+
+    def __init__(self, balance: List[int]):
+        self.balance =  balance
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if account1  > len(self.balance) or account2 > len(self.balance):
+            return False
+        if self.balance[account1 - 1] >= money:
+            self.balance[account1 - 1] -= money
+            self.balance[account2 - 1] += money
+            return True
+        return False
+
+
+    def deposit(self, account: int, money: int) -> bool:
+        if account > len(self.balance):
+            return False
+        self.balance[account - 1] += money
+        return True
+
+    def withdraw(self, account: int, money: int) -> bool:
+        if account > len(self.balance):
+            return False
+        if self.balance[account - 1] >= money:
+            self.balance[account - 1] -= money
+            return True
+        return False
