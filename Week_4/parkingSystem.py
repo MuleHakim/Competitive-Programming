22eaf16b737b0eaeeac3dd3e63c238d80ec3e6f4
MuleHakim <mulukenhakim97@gmail.com>
2023-01-09 23:38:36 +0300

    parking system

---
+++ Week_4/parkingSystem.py
@@ -0,0 +1,30 @@
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.big = big
+        self.medium = medium
+        self.small = small
+
+    def addCar(self, carType: int) -> bool:
+        if carType == 1:
+            if self.big:
+                self.big -= 1
+                return True
+            else: return False
+
+        elif carType == 2:
+            if self.medium:
+                self.medium -= 1
+                return True
+            else: return False
+            
+        else:
+            if self.small:
+                self.small -= 1
+                return True
+            else: return False
+
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
