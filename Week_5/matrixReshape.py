5ce703c01526d4e766e6fa4d72c54e784b4f5182
MuleHakim <mulukenhakim97@gmail.com>
2023-01-14 11:49:29 +0300

    matrix reshape

---
+++ Week_5/matrixReshape.py
@@ -0,0 +1,14 @@
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+
+        m = len(mat)
+        n = len(mat[0])
+        reshaped = [[0]*c for _ in range(r)]
+        
+        counter = 0
+        for row in range(m):
+            for col in range(n):
+                reshaped[counter // c][counter % c] = mat[row][col]
+                counter += 1
+        
+        return reshaped
