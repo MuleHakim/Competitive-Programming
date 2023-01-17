a29ae1dad1b975a57136365c56130ed5b141b2e9
MuleHakim <mulukenhakim97@gmail.com>
2023-01-17 16:15:28 +0300

    mountain aray\

---
+++ Week_6/validMountainArray.py
@@ -0,0 +1,18 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+
+        controller = True
+        if len(arr) < 3: return False
+        if arr[1] < arr[0]: return False
+
+        for i in range(1,len(arr)):
+
+            if arr[i] == arr[i - 1]: return False
+
+            if controller:
+                if arr[i] < arr[i - 1]: controller = False
+
+            if not controller:
+                if arr[i] > arr[i - 1]: return False
+                
+        return controller == False
