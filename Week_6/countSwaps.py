5d916eb7bcfff988f6142d522152709e20a625cf
MuleHakim <mulukenhakim97@gmail.com>
2023-01-18 22:48:50 +0300

    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

---
+++ Week_6/countSwaps.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countSwaps' function below.
+#
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def countSwaps(a):
+    counter = 0
+    for i in range(len(a)):
+        for j in range(len(a) - 1 - i):
+            if a[j] > a[j+1]:
+                a[j], a[j+1] = a[j+1], a[j]
+                counter += 1
+    print(f"Array is sorted in {counter} swaps.")
+    print(f"First Element: {a[0]}")
+    print(f"Last Element: {a[-1]}")
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)
+
