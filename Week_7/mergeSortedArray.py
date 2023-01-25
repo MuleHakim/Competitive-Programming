d77969c94c4a772e9a0dfad69b4731c685dbe02a
MuleHakim <mulukenhakim97@gmail.com>
2023-01-25 23:26:49 +0300

    merge sorted array

---
+++ Week_7/mergeSortedArray.py
@@ -0,0 +1,15 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+
+        pointer1 = m - 1
+        pointer2 = n - 1
+        pointer3 = m + n - 1
+        while pointer2 >= 0:
+            if pointer1 >= 0 and nums1[pointer1] > nums2[pointer2]:
+                nums1[pointer3] = nums1[pointer1]
+                pointer3 -= 1
+                pointer1 -= 1
+            else:
+                nums1[pointer3] = nums2[pointer2]
+                pointer3 -= 1
+                pointer2 -= 1
