60adfc5e1be293f0efc2423310067680aa78ef35
MuleHakim <mulukenhakim97@gmail.com>
2023-01-28 16:44:34 +0300

    2491. Divide Players Into Teams of Equal Skill

---
+++ Week_7/dividePlayers.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+        skill.sort()
+        pre_team = skill[0] + skill[-1]
+        sum_teams = 0
+        left, right = 0, len(skill) - 1
+        
+        while left <= right:
+            if skill[left] + skill[right] != pre_team:
+                return -1
+            sum_teams += skill[left] * skill[right]
+            left += 1
+            right -= 1
+            
+        return sum_teams
