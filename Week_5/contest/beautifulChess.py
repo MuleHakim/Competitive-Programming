e50f625a70a72911d4a9d50cbf79eee2af13e939
Muluken Hakim <91600753+MuleHakim@users.noreply.github.com>
2023-02-20 17:13:09 +0300

    beautifull chess

--- Week_5/contest/beautifulChess.py
+++ Week_5/contest/beautifulChess.py
@@ -1,3 +1,21 @@
+# Method 1
+def beautifulChess():
+    matrix = []
+    for _ in range(8):
+        arr = list(input().strip())
+        matrix.append(arr)
+    for i in range(6):
+        if matrix[i].count("#") == 2 and matrix[i + 1].count("#") == 1 and matrix[i + 2].count("#"):
+            row = i + 1
+            col = matrix[i + 1].index("#")
+            return str(row + 1) + " " + str(col + 1)
+if __name__ == '__main__':
+    testCase = int(input())
+    for _ in range(testCase):
+        input()
+        print(beautifulChess())
+
+# Method 2
 from collections import defaultdict
 def beautifulChess(t):
     for _ in range(t):
@@ -28,8 +46,7 @@ def beautifulChess(t):
         sortedValues = sorted(dict_2, key=lambda k: len(dict_2[k]), reverse=True)
         values = dict_2[sortedValues[0]]
         allDiag += values
- 
- 
+
         ans = ""
         for val in allDiag:
             if allDiag.count(val) == 2:
