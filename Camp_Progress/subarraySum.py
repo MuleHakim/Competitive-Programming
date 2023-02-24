6ffc40a0303836df0ca90eaf3eae68890f716b5b
MuleHakim <mulukenhakim97@gmail.com>
2023-02-24 21:55:34 +0300

    Subarray Sum Equals K

--- Camp_Progress/subarraySum.py
+++ Camp_Progress/subarraySum.py
@@ -1,15 +1,16 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        output = 0
+        count = 0
         total = 0
-        hash = defaultdict(int)
+        my_dic = defaultdict(int)
+        
         for i in range(len(nums)):
             total += nums[i]
             if total == k:
-                output += 1
-            if total-k in hash:
-                output += hash[total-k]
+                count += 1
+            if total-k in my_dic:
+                count += my_dic[total-k]
 
-            hash[total] += 1
+            my_dic[total] += 1
 
-        return output
+        return count
