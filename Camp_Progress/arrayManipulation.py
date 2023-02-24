54e1058bce543838f6792e34acb7c090db145450
Muluken Hakim <91600753+MuleHakim@users.noreply.github.com>
2023-02-24 21:57:53 +0300

    Array manipulation

---
+++ Camp_Progress/arrayManipulation.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def arrayManipulation(n, queries):
+    prefix = [0] * n
+    for i in range(len(queries)):
+        left, right, val = queries[i]
+        prefix[left-1] += val 
+        if right < n:
+            prefix[right] -= val
+            
+    for j in range(1,n):
+        prefix[j] += prefix[j-1]
+        
+    return max(prefix)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
