2f16b63f1b4014ef90dc0ada57fc2f5be54c7617
MuleHakim <mulukenhakim97@gmail.com>
2023-02-28 14:47:29 +0300

    Power of Three

---
+++ Camp_Progress/isPowerOfThree.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n < 1:
+            return False
+        if n == 1:
+            return True
+        return self.isPowerOfThree(n / 3)
