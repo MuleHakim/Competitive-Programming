91da7571a1e2d219261fa1466d0ffd7aa2fe211e
MuleHakim <mulukenhakim97@gmail.com>
2023-02-28 15:16:09 +0300

    Pow(x, n)

---
+++ Camp_Progress/myPow.py
@@ -0,0 +1,9 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if x == 0:
+            return 0
+        if n == 0:
+            return 1
+        if n == 1:
+            return x
+        return pow(x, n)
