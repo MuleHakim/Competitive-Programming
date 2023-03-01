2d036c19a8a68c80354d2777e18c2d7569e4103f
MuleHakim <mulukenhakim97@gmail.com>
2023-03-01 23:26:07 +0300

    Reverse String

--- Camp_Progress/reverseString.py
+++ Camp_Progress/reverseString.py
@@ -1,16 +1,21 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         # recursion
-
-        # if len(s) < 2:
-        #     return s
-        # return self.reverseString(s[len(s)//2:]) + self.reverseString(s[:len(s)//2])
-
-        # two pointers
-
         left, right = 0, len(s) - 1
 
-        while left < right:
-            s[left], s[right] = s[right], s[left]
-            left += 1
-            right -= 1
+        def helper(l, r):
+            if l >= r:
+                return
+            s[l], s[r] = s[r], s[l]
+            helper(l + 1, r - 1)
+            
+        helper(left, right)
+
+
+        # two pointers
+        # left, right = 0, len(s) - 1
+
+        # while left < right:
+        #     s[left], s[right] = s[right], s[left]
+        #     left += 1
+        #     right -= 1
