f9677d545951c9929b9803d0ee2bee0a4271ff95
MuleHakim <mulukenhakim97@gmail.com>
2023-03-03 17:22:48 +0300

    Capacity To Ship Packages Within D Days

--- Camp_Progress/shipWithinDays.py
+++ Camp_Progress/shipWithinDays.py
@@ -1,20 +1,19 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        left = max(weights)
-        right = sum(weights)
-        while left < right:
+        low = max(weights)
+        high = sum(weights)
+        while low < high:
             day = 1
             wt_day = 0
-            mid = (left + right) // 2
+            capacity = (low + high) // 2
             for weight in weights:
-                if wt_day + weight > mid:
+                if wt_day + weight > capacity:
                     day += 1
                     wt_day = 0
                 wt_day += weight
                 
             if day <= days:
-                right = mid
+                high = capacity
             else:
-                left = mid + 1
-                
-        return left
+                low = capacity + 1
+        return low
