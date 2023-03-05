af2612f54dc3a5e7c8f76abbd30892324148f5f6
MuleHakim <mulukenhakim97@gmail.com>
2023-03-05 10:49:45 +0300

    Peak Index in a Mountain Array

---
+++ Camp_Progress/peakIndexInMountainArray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        low, high = 0, len(arr) - 1 
+        while low < high:
+            mid = low + (high - low) // 2
+            if arr[mid] < arr[mid + 1]:
+                low = mid + 1
+            else:
+                high = mid
+        return low
