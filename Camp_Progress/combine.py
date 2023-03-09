d7106e85ae5cb07f3cc15696dc5528431e59a63b
MuleHakim <mulukenhakim97@gmail.com>
2023-03-09 11:59:05 +0300

    Combinations

---
+++ Camp_Progress/combine.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        self.ans = []
+        self.cur = []
+        def backtrack(l):
+            if len(self.cur) == k:
+                self.ans.append(self.cur[:])
+                return 
+                
+            for candidate in range(1, n + 1):
+                if candidate in self.cur:
+                    continue
+                if self.cur and candidate <= self.cur[-1]:
+                    continue
+                self.cur.append(candidate)
+                backtrack(l+1)
+                self.cur.pop()
+
+        backtrack(0)
+        return self.ans
