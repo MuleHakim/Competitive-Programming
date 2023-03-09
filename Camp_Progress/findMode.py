c40390260fa3a56fffbb147ecc075191092fa10d
MuleHakim <mulukenhakim97@gmail.com>
2023-03-09 07:02:28 +0300

    Find Mode in Binary Search Tree

--- Camp_Progress/findMode.py
+++ Camp_Progress/findMode.py
@@ -4,6 +4,26 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# without extra space
+class Solution:
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        def inOrder(node):
+            if node:
+                inOrder(node.left)
+                self.counter[node.val] += 1
+                inOrder(node.right)
+
+        self.counter = Counter()
+        self.ans = []
+        inOrder(root)
+
+        for val,count in self.counter.items():
+            if count == max(self.counter.values()):
+                self.ans.append(val)
+
+        return self.ans
+# with extra space
 class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
         def inOrder(node):
