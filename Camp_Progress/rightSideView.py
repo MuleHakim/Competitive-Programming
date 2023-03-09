756dc52a879037e529f5a63df6f2c3ea66e2d607
MuleHakim <mulukenhakim97@gmail.com>
2023-03-09 22:39:52 +0300

    Binary Tree Right Side View

--- Camp_Progress/rightSideView.py
+++ Camp_Progress/rightSideView.py
@@ -4,6 +4,8 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# using recursion (dfs)
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
 
@@ -20,3 +22,29 @@ class Solution:
         dfs(root,0)
         
         return self.ans
+    
+# using iterative
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root: return []
+        
+        queue = [root]
+        ans = []
+        
+        while queue:
+            cur_level = []
+            length = len(queue)
+
+            while length > 0:
+                cur = queue.pop(0)
+                cur_level.append(cur.val)
+                length -= 1
+
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+
+            ans.append(cur_level[-1])
+
+        return ans
