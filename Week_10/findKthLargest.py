7a0ffac90a118fd68ade901305e3e5317819e1db
MuleHakim <mulukenhakim97@gmail.com>
2023-03-23 23:08:07 +0300

    Kth Largest Element in an Array

---
+++ Week_10/findKthLargest.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        def quickSort(low,high):
+            while low < high:
+                mid = (low + high) // 2
+                pivot_idx = max(min(low, mid, high), min(max(low, mid), high), min(low, max(mid, high)))
+                nums[pivot_idx], nums[high] = nums[high], nums[pivot_idx]
+                pivot = nums[high]
+
+                i = low - 1
+                for j in range(low, high):
+                    if nums[j] <= pivot:
+                        i = i + 1
+                        nums[i], nums[j] = nums[j], nums[i]
+
+                nums[i + 1], nums[high] = nums[high], nums[i + 1]
+                quickSort(low, i)
+                quickSort(i+2, high)
+                return
+
+        quickSort(0,len(nums)-1)
+
+        return nums[len(nums)-k]
