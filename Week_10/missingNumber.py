de97b5100597ea5a0dcc9d48ce637b866c3061e6
MuleHakim <mulukenhakim97@gmail.com>
2023-03-26 01:26:54 +0300

    Missing Number

--- Week_10/missingNumber.py
+++ Week_10/missingNumber.py
@@ -1,14 +1,15 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        for i in range(len(nums)):
-            while nums[i] != i:
-                if nums[i] != len(nums):
-                    nums[nums[i]], nums[i] = nums[i], nums[nums[i]]
-                else:
-                    break
+        i = 0
+        while i < len(nums):
+            j = nums[i]
+            if j != len(nums) and j != i:
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                i += 1
 
         for i in range(len(nums)):
             if nums[i] != i:
                 return i
-                
+
         return len(nums)
