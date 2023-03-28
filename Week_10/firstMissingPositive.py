42f681679093bbce07b09e27c7b6691285aa6b4a
MuleHakim <mulukenhakim97@gmail.com>
2023-03-28 23:55:42 +0300

    First Missing Positive

---
+++ Week_10/firstMissingPositive.py
@@ -0,0 +1,13 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+
+        for i in range(len(nums)):
+            while 1 <= nums[i] <= len(nums) and nums[nums[i] - 1] != nums[i]:
+                j = nums[i] - 1
+                nums[i], nums[j] = nums[j], nums[i]
+                
+        for i in range(len(nums)):
+            if nums[i] != i+1:
+                return i+1
+                
+        return len(nums) + 1
