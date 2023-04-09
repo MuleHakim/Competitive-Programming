87e9a80ecf0d0132561f669ec1cd23933e45074d
MuleHakim <mulukenhakim97@gmail.com>
2023-04-09 21:53:34 +0300

    """ # Definition for Employee. class Employee:     def __init__(self, id: int, importance: int, subordinates: List[int]):         self.id = id         self.importance = importance         self.subordinates = subordinates """
    class Solution:
        def getImportance(self, employees: List[Employee], id: int) -> int:
            graph = defaultdict(list)
            value = defaultdict(int)
            for employee in employees:

---
+++ Week_11/getImportance.py
@@ -0,0 +1,25 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        graph = defaultdict(list)
+        value = defaultdict(int)
+        for employee in employees:
+            graph[employee.id] = employee.subordinates
+            value[employee.id] = employee.importance
+
+        def dfs(node,graph):
+            self.total += value[node] 
+            for neighbor in graph[node]:
+                dfs(neighbor,graph)
+                
+        self.total = 0
+        dfs(id,graph)
+        return self.total
