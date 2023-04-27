a7b2cf44db80f7dd1e02af518d376a6ac59132c7
MuleHakim <mulukenhakim97@gmail.com>
2023-04-27 14:43:49 +0300

    Keys and Rooms

--- Week_13/canVisitAllRooms.py
+++ Week_13/canVisitAllRooms.py
@@ -1,8 +1,31 @@
+# BFS
+
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         graph = defaultdict(list)
-        for i,room in enumerate(rooms):
-            graph[i] = room
+        for key,room in enumerate(rooms):
+            graph[key] = room
+        visited = set()
+        queue = deque()
+        def bfs(key):
+            visited.add(key)
+            queue.append(key)
+            while queue:
+                room = queue.popleft()
+
+                for key in graph[room]:
+                    if key not in visited:
+                        visited.add(key)
+                        queue.append(key)
+        bfs(0)
+        return True if len(visited)==len(rooms) else False
+
+# DFS
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+        for key,room in enumerate(rooms):
+            graph[key] = room
             
         visited = set()
         def dfs(key):
