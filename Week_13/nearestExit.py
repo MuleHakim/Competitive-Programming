a6c737c86d22158707a3702c0d4e7e5991b98161
MuleHakim <mulukenhakim97@gmail.com>
2023-05-03 19:01:42 +0300

    Nearest Exit from Entrance in Maze

--- Week_13/nearestExit.py
+++ Week_13/nearestExit.py
@@ -12,14 +12,13 @@ class Solution:
             r, c, step = queue.popleft()
             a  = 0
             for i,j in [(r,c+1),(r,c-1),(r+1,c),(r-1,c)]:
-                if not inbound(i,j) and [r,c] != entrance: a += 1
+                if not inbound(i,j) and [r,c] != entrance:
+                    return step
                     
-            if a >= 1: ans.add(step)
-
             directions = [(r,c+1),(r,c-1),(r+1,c),(r-1,c)]
             for x,y in directions:
                 if inbound(x,y) and maze[x][y] == ".":
                     maze[x][y] = "+"
                     queue.append((x,y,step+1))
 
-        return min(ans) if ans else -1
+        return -1
