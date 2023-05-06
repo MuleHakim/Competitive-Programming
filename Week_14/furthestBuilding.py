8f0bcdbe6f8bab0a69a4493f2257aa8b03d3f072
MuleHakim <mulukenhakim97@gmail.com>
2023-05-06 23:59:26 +0300

    Furthest Building You Can Reach

---
+++ Week_14/furthestBuilding.py
@@ -0,0 +1,13 @@
+class Solution:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        heap = []
+        for i in range(len(heights)-1):
+            diff = heights[i+1] - heights[i]
+            if diff > 0:
+                heapq.heappush(heap, diff)
+            if len(heap) > ladders:
+                bricks -= heapq.heappop(heap)
+            if bricks < 0:
+                return i
+        
+        return len(heights) - 1
