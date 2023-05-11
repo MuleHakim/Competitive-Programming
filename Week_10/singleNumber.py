2f130fe0976795ac8f1eb5268252217a5e745b4d
MuleHakim <mulukenhakim97@gmail.com>
2023-05-11 10:53:30 +0300

    Single Number II

--- Week_10/singleNumber.py
+++ Week_10/singleNumber.py
@@ -1,3 +1,22 @@
+# O(32n)
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = 0
+        shift = 1
+        for i in range(32):
+            count = 0
+            for num in nums:
+                if num & shift:
+                    count += 1
+            if count % 3 != 0:
+                if i == 31: 
+                    ans -= (2**31)
+                else:
+                    ans += shift
+            shift *= 2
+            
+        return ans
+# O(n)
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         ones, twos = 0, 0
